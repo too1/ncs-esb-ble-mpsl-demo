2ed5f2336bb293d27034d7676d5bf9c1ed929d04
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2023-01-20 13:44:32 +0100

    Stripping out superfluous timeslot code, this demo will only require continuous extended timeslots

--- src/main.c
+++ src/main.c
@@ -49,15 +49,10 @@ uint16_t number_of_times_extended = 0;
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-static bool user_request_extension_fail = false; 
-static bool request_multi_fire = false;
 static bool request_extension = false; 
 
 static uint32_t extension_time_us = 5000;
-static uint32_t invalid_extension_time_us = MPSL_TIMESLOT_EXTENSION_TIME_MIN_US*0;
 static uint8_t timeslot_number = 0;
-static uint8_t firing_number = 0;
-
 
 /* MPSL API calls that can be requested for the non-preemptible thread */
 enum mpsl_timeslot_call {
@@ -74,14 +69,6 @@ static mpsl_timeslot_request_t timeslot_request_earliest = {
 	.params.earliest.length_us = TIMESLOT_LENGTH_US,
 	.params.earliest.timeout_us = 1000000
 };
-static mpsl_timeslot_request_t timeslot_request_normal = {
-	.request_type = MPSL_TIMESLOT_REQ_TYPE_NORMAL,
-	.params.normal.hfclk = MPSL_TIMESLOT_HFCLK_CFG_NO_GUARANTEE,
-	.params.normal.priority = MPSL_TIMESLOT_PRIORITY_NORMAL,
-	.params.normal.distance_us = TIMESLOT_REQUEST_DISTANCE_US,
-	.params.normal.length_us = TIMESLOT_LENGTH_US
-};
-
 
 static mpsl_timeslot_signal_return_param_t signal_callback_return_param;
 
@@ -172,18 +159,6 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 			nrf_timer_cc_set(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, TIMER_EXPIRY_US_EARLY);
 			nrf_timer_int_enable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
 		}
-		else if(request_multi_fire){
-			nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
-			nrf_timer_cc_set(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, TIMER_EXPIRY_US_MULTI);
-			nrf_timer_int_enable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
-
-		}
-		else {
-			/*Extension not requested. CC in place to tell that session needs to end or that new session should be requested */
-			nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
-			nrf_timer_cc_set(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, TIMER_EXPIRY_US_EARLY);
-			nrf_timer_int_enable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
-		}
 		input_data_len = ring_buf_put(&callback_high_priority_ring_buf, &input_data, 1);
 		if (input_data_len != 1) {
 			LOG_ERR("Full ring buffer, enqueue data with length %d", input_data_len);
@@ -196,54 +171,11 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 
 	case MPSL_TIMESLOT_SIGNAL_TIMER0:
 		/* Clear event */
-		if (!request_multi_fire){
-			nrf_timer_int_disable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
-			nrf_timer_event_clear(NRF_TIMER0, NRF_TIMER_EVENT_COMPARE0);
-		}
+		nrf_timer_int_disable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
+		nrf_timer_event_clear(NRF_TIMER0, NRF_TIMER_EVENT_COMPARE0);
 
-		if (request_in_cb && !request_extension && !request_multi_fire) { 
-			/* Request new timeslot when callback returns. This should occur TIMER_EXPIRY_US after timeslot began*/
-
-			signal_callback_return_param.params.request.p_next = 
-				&timeslot_request_normal;
-			signal_callback_return_param.callback_action =
-				MPSL_TIMESLOT_SIGNAL_ACTION_REQUEST;
-			LOG_INF("Requesting normal slot..\n");
-
-		}
-		else if (request_in_cb && request_extension && !request_multi_fire) {
-			/* Request extension. This occurs TIMER_EXPIRY_US_EARLY after timeslot began  */
-			if (user_request_extension_fail && number_of_times_extended==10){ /* However in this case we want it to fail */
-				signal_callback_return_param.params.extend.length_us = invalid_extension_time_us;
-			}
-			else{
-				signal_callback_return_param.params.extend.length_us = extension_time_us; 
-			}
-
-			signal_callback_return_param.callback_action =
-				MPSL_TIMESLOT_SIGNAL_ACTION_EXTEND;
-
-		}
-		else if(request_multi_fire){
-			/* Set new timer */
-			nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
-			nrf_timer_cc_set(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, TIMER_EXPIRY_US_MULTI);
-			nrf_timer_int_enable(NRF_TIMER0, NRF_TIMER_INT_COMPARE0_MASK);
-
-			LOG_INF("This is timeslot number: %d, and firing number: %d\n", timeslot_number, firing_number+=1);
-
-			/* Timeslot will be ended */
-			signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
-		}
-
-		else {
-			/* Timeslot will be ended */
-			signal_callback_return_param.callback_action =
-				MPSL_TIMESLOT_SIGNAL_ACTION_END;
-			display_timeslot(false);
-			LOG_INF("Not requesting extension..\n");
-
-		}
+		signal_callback_return_param.params.extend.length_us = extension_time_us; 
+		signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_EXTEND;
 
 		p_ret_val = &signal_callback_return_param;
 		input_data_len = ring_buf_put(&callback_high_priority_ring_buf, &input_data, 1);
@@ -253,10 +185,9 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 		}
 		break;
 
-//added cases:
 	case MPSL_TIMESLOT_SIGNAL_EXTEND_SUCCEEDED:
-		signal_callback_return_param.callback_action =
-			MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
+		signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
+
 		/* Set next trigger time to be the current + Timer expiry early */
 		uint32_t current_cc = nrf_timer_cc_get(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0);
 		uint32_t next_trigger_time = current_cc + extension_time_us;
@@ -269,31 +200,17 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 													current_cc, 
 													next_trigger_time);
 
-		display_timeslot(true);
+		//display_timeslot(true);
 		p_ret_val = &signal_callback_return_param;
 
 		break;
 
 	case MPSL_TIMESLOT_SIGNAL_EXTEND_FAILED:
 		LOG_INF("Extension failed!");
-		if (user_request_extension_fail){
-			signal_callback_return_param.params.request.p_next = 
-				&timeslot_request_earliest;
-			signal_callback_return_param.callback_action =
-				MPSL_TIMESLOT_SIGNAL_ACTION_REQUEST;
-			LOG_INF("..requesting normal slot..\n");
-			request_extension = false;
-		}
-		else{
-			signal_callback_return_param.callback_action =
-				MPSL_TIMESLOT_SIGNAL_ACTION_END;
-		
-		}
+		signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_END;
 
 		p_ret_val = &signal_callback_return_param;
-
 		display_timeslot(false);
-
 		break;
 
 	case MPSL_TIMESLOT_SIGNAL_RADIO:
@@ -344,8 +261,7 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 			LOG_ERR("Full ring buffer, enqueue data with length %d", input_data_len);
 			k_oops();
 		}
-		signal_callback_return_param.callback_action = 
-			MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
+		signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
 		p_ret_val = &signal_callback_return_param;
 		display_timeslot(false);
 		break;
@@ -356,14 +272,11 @@ static mpsl_timeslot_signal_return_param_t *mpsl_timeslot_callback(
 		if (input_data_len != 1) {
 			LOG_ERR("Full ring buffer, enqueue data with length %d", input_data_len);
 			k_oops();
-		p_ret_val = &signal_callback_return_param;
-
+			p_ret_val = &signal_callback_return_param;
 		}
-		signal_callback_return_param.callback_action =
-			MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
+		signal_callback_return_param.callback_action = MPSL_TIMESLOT_SIGNAL_ACTION_NONE;
 		display_timeslot(false);
 		break;
-
 
 	default:
 		LOG_ERR("unexpected signal: %u", signal_type);
@@ -409,13 +322,6 @@ static void mpsl_timeslot_demo(void)
 	} else if (input_char == 'c') {
 		request_in_cb = true;
 		request_extension = true;
-	} else if (input_char == 'd') {
-		request_in_cb = true;
-		request_extension = true;
-		user_request_extension_fail = true;
-	} else if (input_char == 'e') {
-		request_multi_fire = true;
-		timeslot_number=0;
 	} else {
 		return;
 	}
